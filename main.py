89695c59a0d4c992eed8c35730a5959eefae9a44
Gregorop <Gregorop@yandex.ru>
2023-04-13 19:23:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from random import randint
+
+def test():
+    text.setText(str(randint(0, 100)))
+
+app = QApplication([])
+win = QWidget()
+
+line = QVBoxLayout()
+
+text = QLabel()
+line.addWidget(text, alignment= Qt.AlignCenter)
+
+random_btn = QPushButton('выбрать победителя')
+random_btn.clicked.connect(test)
+line.addWidget(random_btn)
+
+win.setLayout(line) #устанавливаем линию в окно
+win.show()
+app.exec()
